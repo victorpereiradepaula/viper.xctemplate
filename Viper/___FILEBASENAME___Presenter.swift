6f307e5a6aca6a3b367a255e687f55c93728f8af
Victor Pereira <victorpereira@jera.com.br>
2018-10-29 12:17:12 -0400

    fixed

--- Viper/___FILEBASENAME___Presenter.swift
+++ Viper/___FILEBASENAME___Presenter.swift
@@ -13,8 +13,6 @@ class ___FILEBASENAMEASIDENTIFIER___: BasePresenter {
     weak var router: ___VARIABLE_productName:identifier___WireFrameProtocol?
     private let interactor: ___VARIABLE_productName:identifier___InteractorProtocol
     
-    private let disposeBag = DisposeBag()
-    
     init(interactor: ___VARIABLE_productName:identifier___InteractorProtocol) {
         self.interactor = interactor
         super.init()
