b79b60fe6429eff1a1d1f36f67fe3e4469b9a9f1
Victor Pereira <victorpereira@jera.com.br>
2019-06-20 15:03:22 -0400

    apiClient remove

--- ViperBuilder + IGListKit/___FILEBASENAME___Factory.swift
+++ ViperBuilder + IGListKit/___FILEBASENAME___Factory.swift
@@ -5,7 +5,7 @@
 enum ___FILEBASENAMEASIDENTIFIER___ {
    
    static func build(router: ___VARIABLE_productName:identifier___RouterProtocol) -> UIViewController {
-      let repository = ___VARIABLE_productName:identifier___Repository(apiClient: APIClient())
+      let repository = ___VARIABLE_productName:identifier___Repository()
       let interactor = ___VARIABLE_productName:identifier___Interactor(repository: repository)
       let presenter = ___VARIABLE_productName:identifier___Presenter(interactor: interactor)
       presenter.router = router
