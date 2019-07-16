d6a64869fca770e9ac9184363e4d2db50ba231d7
Victor Pereira <victorpereira@jera.com.br>
2019-07-16 15:07:11 -0400

    fixed

--- ViperBuilder/___FILEBASENAME___Presenter.swift
+++ ViperBuilder/___FILEBASENAME___Presenter.swift
@@ -14,7 +14,7 @@ public final class ___FILEBASENAMEASIDENTIFIER___: BasePresenter {
     weak var router: ___VARIABLE_productName:identifier___RouterProtocol?
     private let interactor: ___VARIABLE_productName:identifier___InteractorProtocol
     
-    init(interactor: ___VARIABLE_productName:identifier___InteractorProtocol) {
+    public init(interactor: ___VARIABLE_productName:identifier___InteractorProtocol) {
         self.interactor = interactor
         
         super.init()
